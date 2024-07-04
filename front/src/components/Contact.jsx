d593bd8fd1591e1179b459bebd6785d93ef17f24
Omar <>
2024-07-04 04:09:21 +0400

    added contact form functionality with simple json server

--- front/src/components/Contact.jsx
+++ front/src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import ContactForm from "./ContactForm";
 import {Scramble} from "./Scramble.js";
+const SERVER_URL = 'http://localhost:3001/contactforms'
+
 
 const Contact = () => {
     return (
@@ -15,7 +17,7 @@ const Contact = () => {
                         If you'd prefer, you can also contact me at <u>
                         <a className="text-accent1 hover:text-accent1-hover" href={Scramble('lc158d7dlcxdlb12xc34lc15b6dl')}>{Scramble('dlcxdlb12xc34lc15b6dl')}</a></u>
                     </p>
-                    <ContactForm/>
+                    <ContactForm who = "omar" ></ContactForm>
                 </div>
                 {/*Arun*/}
                 <div className="px-10 py-10 w-[90vw] sm:w-[60vw] mx-auto bg-primary1-hover rounded-[30px] mt-10">
@@ -28,7 +30,7 @@ const Contact = () => {
                            href={Scramble('lc158d7bbb34lc15b6dl')}>{Scramble('bbb34lc15b6dl')}</a></u>
                     </p>
                     <div>
-                        <ContactForm/>
+                        <ContactForm who = "arun" ></ContactForm>
                     </div>
                 </div>
             </div>
--- front/src/components/ContactForm.jsx
+++ front/src/components/ContactForm.jsx
@@ -1,66 +1,81 @@
 import React, { useState } from "react";
-const FORM_ENDPOINT = "https://herotofu.com/start"; // TODO - update to the correct endpoint
+import axios from 'axios'
 
-const ContactForm = () => {
+const ContactForm = ({who}) => {
+    const [submitted, setSubmitted] = useState(0)  // 0 = not submitted, 1 = submitted successfully, 2 = submitted but unsuccessful
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
 
-    const [submitted, setSubmitted] = useState(false);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const inputs = e.target.elements;
-        const data = {};
-
-        for (let i = 0; i < inputs.length; i++) {
-            if (inputs[i].name) {
-                data[inputs[i].name] = inputs[i].value;
-            }
+    const addForm = event => {
+        event.preventDefault()
+        const formObject = {
+            id: name,
+            name: name,
+            email: email,
+            message: message,
         }
 
-        console.log(data);
-        setSubmitted(true);
-    };
+        const promise = axios.post(`http://localhost:3001/${who}`, formObject)
+        promise.then(_ => {
+            setSubmitted(1)
+        })
 
+        promise.catch(_ => {
+                setSubmitted(2)
+        })
+    }
 
-    if (submitted) {
+    if (submitted === 1) {  // submitted successfully
         return (<div>
                 <div className="text-2xl">Thank you! ☺</div>
                 <div className="text-md">I'll get back to you as soon as I can.</div>
             </div>);
     }
+    else {  // not submitted or unsuccessful
+        return (<form onSubmit={(event) => addForm(event)}>
+                <div>
+                    <p>{(submitted === 2) ? "An error prevented your form from submitting, please try again or send an email if the issue persists" : ""}</p>
+                </div>
 
+                <div className="pt-0 mb-3">
+                    <input type="text" placeholder="Your name" name="name"
+                           className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-pinkbeige border-0 rounded shadow outline-none"
+                           required
+                            onChange={(event) => setName(event.target.value)}/>
+                </div>
 
-    return (<form action={FORM_ENDPOINT} onSubmit={handleSubmit} method="POST">
-            <div className="pt-0 mb-3">
-                <input type="text" placeholder="Your name" name="name" className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-pinkbeige border-0 rounded shadow outline-none" required/>
-            </div>
+                <div className="pt-0 mb-3">
+                    <input type="email" placeholder="Your email" name="email"
+                           className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-pinkbeige border-0 rounded shadow outline-none"
+                           required
+                           onChange={(event) => setEmail(event.target.value)}/>
+                </div>
 
-            <div className="pt-0 mb-3">
-                <input type="email" placeholder="Email" name="email" className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-pinkbeige border-0 rounded shadow outline-none" required/>
-            </div>
+                <div className="pt-0 mb-3">
 
-            <div className="pt-0 mb-3">
+            <textarea
+                placeholder="Your message"
+                name="message"
+                className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-pinkbeige border-0 rounded shadow outline-none"
+                required
+                onChange={(event) => setMessage(event.target.value)}
+            />
 
-        <textarea
-            placeholder="Your message"
-            name="message"
-            className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-pinkbeige border-0 rounded shadow outline-none"
-            required
-        />
+                </div>
 
-            </div>
+                <div className="mb-3 stroke-pinkbeige">
+                    <button
+                        className="text-pinkbeige px-6 py-3 mb-1 mr-1 text-sm font-bold text-pinkbeige transition-all duration-150 ease-linear rounded"
+                        type="submit">
+                        <p>Submit</p>
+                    </button>
 
-            <div className="mb-3 stroke-pinkbeige">
-                <button
-                    className="text-pinkbeige px-6 py-3 mb-1 mr-1 text-sm font-bold text-pinkbeige transition-all duration-150 ease-linear rounded"
-                    type="submit">
-                    <p>Submit</p>
-                </button>
+                </div>
+            </form>
 
-            </div>
-        </form>
-
-    );
+        );
+    }
 
 };
 
